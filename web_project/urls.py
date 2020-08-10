005d1f370589e1b5d3144ffca24488ab1310e4a1
Ohsanghun <otter.oh@gmail.com>
2020-08-10 15:41:05 +0900

    add selenium

--- web_project/urls.py
+++ web_project/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 ]
 
 from crawling.crawling_tasks import task_hello, task_crawling_daum		# add
-task_hello(schedule=20, repeat=59*59*24)
-task_crawling_daum(schedule=10, repeat=59*59*24)
+# task_hello(schedule=20, repeat=59*59*24)
+task_crawling_daum(schedule=0, repeat=59*59*24)
 # task_hello(schedule=20, repeat=60*4)
 # task_crawling_daum(schedule=10, repeat=59*3)
