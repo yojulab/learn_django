f90899b97d5cb0dca1b15bb2f689cfd66fde9c15
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2020-08-22 18:12:09 +0900

    add send keys by selenium

--- scraping/selenium_Keys.py
+++ scraping/selenium_Keys.py
@@ -76,58 +76,8 @@ def download_google_staticimages():
     searchword = searchword1 +' '+searchword2+' '+searchword3
     browser.find_element_by_xpath('//input[@value="cat dog cartoon"]').click()
 
-    # Scroll down 2
-    for i in range(50):
-        element.send_keys(Keys.PAGE_DOWN)
-        time.sleep(0.3)
-
-    try:
-        browser.find_element_by_id('smb').click()
-        for i in range(50):
-            element.send_keys(Keys.PAGE_DOWN)
-            time.sleep(0.3)
-    except:
-        for i in range(10):
-            element.send_keys(Keys.PAGE_DOWN)
-            time.sleep(0.3)
-
-    #elements = browser.find_elements_by_xpath('//div[@id="islrg"]')
-    #page_source = elements[0].get_attribute('innerHTML')
-    page_source = browser.page_source 
-
-    soup = BeautifulSoup(page_source, 'lxml')
-    images = soup.find_all('img')
-
-    urls = []
-    for image in images:
-        try:
-            url = image['data-src']
-            if not url.find('https://'):
-                urls.append(url)
-        except:
-            try:
-                url = image['src']
-                if not url.find('https://'):
-                    urls.append(image['src'])
-            except Exception as e:
-                print(f'No found image sources.')
-                print(e)
-
-    count = 0
-    if urls:
-        for url in urls:
-            try:
-                res = requests.get(url, verify=False, stream=True)
-                rawdata = res.raw.read()
-                with open(os.path.join(dirs, 'img_' + str(count) + '.jpg'), 'wb') as f:
-                    f.write(rawdata)
-                    count += 1
-            except Exception as e:
-                print('Failed to write rawdata.')
-                print(e)
-
     browser.close()
-    return count
+    return i
 
 # Main block
 def main():
